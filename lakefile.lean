9c79b6750c5e355b22b6ccc8d815a856165ade71
FujiHaruka <fizzyjohn1427@gmail.com>
2024-11-04 15:26:32 +0900

    Add mathlib

--- lakefile.lean
+++ lakefile.lean
@@ -4,8 +4,8 @@ open Lake DSL
 package «default» where
   -- add package configuration options here
 
-lean_lib «Default» where
-  -- add library configuration options here
+require mathlib from git
+  "https://github.com/leanprover-community/mathlib4.git"
 
 @[default_target]
 lean_exe «default» where

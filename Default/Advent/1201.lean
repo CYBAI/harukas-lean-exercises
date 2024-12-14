730d3d3e1c37858639bd7388229fbd9835d79f88
FujiHaruka <fizzyjohn1427@gmail.com>
2024-12-14 11:24:28 +0900

    1201.lean part 2

--- Default/Advent/1201.lean
+++ Default/Advent/1201.lean
@@ -1,3 +1,7 @@
+import Std.Data.HashMap
+
+-- Part 1
+
 def sort (l: List Nat) : List Nat :=
   l.mergeSort (· < ·)
 
@@ -10,6 +14,13 @@ def distance_pair (pair: Nat × Nat) : Nat :=
 def distance (zipped: List (Nat × Nat)) : Nat :=
   zipped.foldl (fun acc (x, y) => acc + distance_pair (x, y)) 0
 
+-- Part 2
+
+def frequency_map (list: List Nat): Std.HashMap Nat Nat :=
+  list.foldl (fun acc x => acc.insert x (acc.getD x 0 + 1)) {}
+
+-- Read input
+
 def read_input : IO String := IO.FS.readFile "Default/Advent/1201.txt"
 
 def parse_input (input: String) : ((List Nat) × (List Nat)) :=
@@ -21,10 +32,20 @@ def parse_input (input: String) : ((List Nat) × (List Nat)) :=
   let l2 := lines.map (fun (_, y) => y)
   (l1, l2)
 
-def main : IO Unit := do
+def part1 : IO Unit := do
   let input ← read_input
   let (l1, l2) := parse_input input
   let answer := (sort l1, sort l2) |> Function.uncurry zip |> distance
   IO.println answer
 
-#eval main
+def part2 : IO Unit := do
+  let input ← read_input
+  let (l1, l2) := parse_input input
+  let l1map := frequency_map l1
+  let l2map := frequency_map l2
+
+  let similarity_score := l1map.fold (fun acc => fun k => fun v => acc + (k * v * (l2map.getD k 0))) 0
+  IO.println similarity_score
+
+-- #eval part1
+#eval part2
